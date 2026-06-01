3616d30253b164ce85524f6b7f3c5e711ff6d657
Bereket Aschalew <bereketaschalew1212@gmail.com>
2026-06-01 12:16:56 +0000

    feat(shared): introduce BuildContext extensions for UI ease

---
+++ memere_mobile/lib/shared/extensions/context_extensions.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/app_sizes.dart';
+
+extension BuildContextX on BuildContext {
+  ThemeData get theme => Theme.of(this);
+  TextTheme get textTheme => Theme.of(this).textTheme;
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+  Size get screenSize => MediaQuery.of(this).size;
+  double get screenWidth => MediaQuery.of(this).size.width;
+  double get screenHeight => MediaQuery.of(this).size.height;
+  EdgeInsets get viewPadding => MediaQuery.of(this).viewPadding;
+  bool get isKeyboardOpen => MediaQuery.of(this).viewInsets.bottom > 0;
+
+  void showSnackBar(String message, {bool isError = false}) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: isError ? const Color(0xFFCF6679) : const Color(0xFF4CAF50),
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.all(AppSizes.md),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(AppSizes.radiusSm)),
+      ),
+    );
+  }
+}
