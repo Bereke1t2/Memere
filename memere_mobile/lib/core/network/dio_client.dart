9ad864c7d6d0f1a43a672497762592e46aede753
Bereket Aschalew <bereketaschalew1212@gmail.com>
2026-06-01 12:17:59 +0000

    feat(core): setup base dio network client infrastructure

---
+++ memere_mobile/lib/core/network/dio_client.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: unused_import
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../constants/app_constants.dart';
+import '../constants/env.dart';
+import 'interceptors/auth_interceptor.dart';
+import 'interceptors/logging_interceptor.dart';
+
+final dioClientProvider = Provider<DioClient>((ref) {
+  return DioClient();
+});
+
+class DioClient {
+  DioClient() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: Env.baseUrl,
+        connectTimeout: AppConstants.connectionTimeout,
+        receiveTimeout: AppConstants.receiveTimeout,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      ),
+    );
+    _dio.interceptors.addAll([
+      AuthInterceptor(_dio),
+      LoggingInterceptor(),
+    ]);
+  }
+
+  late final Dio _dio;
+
+  Dio get dio => _dio;
+
+  Future<Response<T>> get<T>(String path, {Map<String, dynamic>? queryParameters}) =>
+      _dio.get<T>(path, queryParameters: queryParameters);
+
+  Future<Response<T>> post<T>(String path, {dynamic data, Map<String, dynamic>? queryParameters}) =>
+      _dio.post<T>(path, data: data, queryParameters: queryParameters);
+
+  Future<Response<T>> put<T>(String path, {dynamic data}) =>
+      _dio.put<T>(path, data: data);
+
+  Future<Response<T>> patch<T>(String path, {dynamic data}) =>
+      _dio.patch<T>(path, data: data);
+
+  Future<Response<T>> delete<T>(String path) =>
+      _dio.delete<T>(path);
+}
